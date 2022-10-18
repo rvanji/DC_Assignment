ce307bfa838628bd7e07f454dfceb9e0e1b9a0e9
rvanji <ravin@LAPTOP-55KCAJ3O>
2022-10-18 07:40:14 +0530

    Added DB Model, Functions, Enums

---
+++ DatabaseWebAPI/Other/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseWebAPI.Other
+{
+    public class Enums
+    {
+        public enum StatusEnums
+        {
+            [Display(Name = "Active")]
+            Active= 1,
+            [Display(Name = "Deactive")]
+            Deactive = 2,
+            [Display(Name = "Pending")]
+            Pending = 3,
+            [Display(Name = "Completed")]
+            Completed = 4,
+            [Display(Name = "Removed")]
+            Removed = 5,
+            [Display(Name = "Passed")]
+            Passed = 6,
+            [Display(Name = "Failed")]
+            Failed = 7,
+        }
+    }
+    
+}
---
+++ DatabaseWebAPI/Other/Functions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseWebAPI.Other
+{
+    public class Functions
+    {
+        public string EncodeString(string text)
+        {
+            if(String.IsNullOrEmpty(text) || String.Equals(text.Trim(), ""))
+                return text;
+
+            byte[] strBytes = System.Text.Encoding.UTF8.GetBytes(text);
+            return Convert.ToBase64String(strBytes);
+        }
+
+        public string DecodeString(string base64Text)
+        {
+            if (String.IsNullOrEmpty(base64Text) || String.Equals(base64Text.Trim(), ""))
+                return base64Text;
+
+            byte[] encodedBytes = Convert.FromBase64String(base64Text);
+            return System.Text.Encoding.UTF8.GetString(encodedBytes);
+        }
+    }
+}
